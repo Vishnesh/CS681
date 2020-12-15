fed3caf5672b9023a2bcf91c8efc5255128f77ca
vishnesh <v.subramanian001@umb.edu>
2020-12-14 20:33:05 -0500

    HW11 Fix

--- HW11/src/edu/umb/cs681/hw11/ConcurrentSingleton.java
+++ HW11/src/edu/umb/cs681/hw11/ConcurrentSingleton.java
@@ -3,6 +3,7 @@ package edu.umb.cs681.hw11;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class ConcurrentSingleton {
+	private ConcurrentSingleton() {}
 	private static AtomicReference<ConcurrentSingleton> instance = new AtomicReference<>(null);
 
 	public static ConcurrentSingleton getInstance() {
--- HW11/src/edu/umb/cs681/hw11/ConcurrentSingletonRunnable.java
+++ HW11/src/edu/umb/cs681/hw11/ConcurrentSingletonRunnable.java
@@ -2,14 +2,7 @@ package edu.umb.cs681.hw11;
 
 public class ConcurrentSingletonRunnable implements Runnable {
 	public void run() {
-		try {
-			for (int i = 0; i < 2; i++) {
-				System.out.println(ConcurrentSingleton.getInstance());
-				Thread.sleep(2);
-			}
-		} catch (InterruptedException ex) {
-			System.out.println(ex);
-		}
+		System.out.println(ConcurrentSingleton.getInstance());
 	}
 
 	public static void main(String[] args) {
